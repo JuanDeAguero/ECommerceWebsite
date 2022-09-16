8b29eddd3a70fa32824b43dfc5616e4bd1c365bf
juandeaguero <juandeaguero@gmail.com>
2022-09-16 15:28:29 -0400

    more navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-
-
+import { Badge } from "@mui/material"
+import { Search, ShoppingCartOutlined } from "@mui/icons-material"
 
 const Navbar = () => {
-	return (
-		<div>
-			Navbar
-		</div>
+    return (
+        <div className="navbar_container">
+            <div className="navbar_wrapper">
+                <div className="navbar_left">
+                    <span className="navbar_language">EN</span>
+                    <div className="navbar_search_container">
+                        <input className="navbar_input"></input>
+                        <Search style={{color: "gray", fontSize: 20}}/>
+                    </div>
+                </div>
+                <div className="navbar_center">
+                    <h1 className="navbar_logo">REACT.</h1>
+                </div>
+                <div className="navbar_right">
+                    <div className="navbar_menu_item">REGISTER</div>
+                    <div className="navbar_menu_item navbar_sign_in">SIGN IN</div>
+                    <div className="navbar_menu_item navbar_cart">
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </div>
+                </div>
+            </div>
+        </div>
 	);
 }
 
